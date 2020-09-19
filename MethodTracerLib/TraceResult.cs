98b13282c54d12ecc0704f541c0f3bf9fb5bd104
Vladzensan <vladzensan@gmail.com>
2020-09-19 21:09:26 +0300

    Tracer implementation

--- MethodTracerLib/TraceResult.cs
+++ MethodTracerLib/TraceResult.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace MethodTracerLib
 {
+
+    class TracedBlock
+    {
+        public Stopwatch watch = new Stopwatch();
+
+        public MethodTrace methodTrace;
+
+        public TracedBlock(MethodTrace methodTrace)
+        {
+            this.methodTrace = methodTrace;
+        }
+    }
+
+    public class MethodTrace
+    {
+        public string methodName { get; set; }
+        public string className { get; set; }
+        public TimeSpan executionTime { get; set; }
+
+        public List<MethodTrace> innerCalls = new List<MethodTrace>();
+    }
+
+    public class ThreadInfo
+    {
+        public TimeSpan executionTime = new TimeSpan(0);
+        public List<MethodTrace> methods = new List<MethodTrace>();
+    }
     public class TraceResult
     {
+        public ConcurrentDictionary<int, ThreadInfo> threads = new ConcurrentDictionary<int, ThreadInfo>();
+
     }
 }
--- MethodTracerLib/Tracer.cs
+++ MethodTracerLib/Tracer.cs
@@ -1,31 +1,71 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace MethodTracerLib
 {
     class Tracer : ITracer
     {
-        private class TracedBlock
-        {
 
-        }
-
-        private ConcurrentDictionary<int, List<TracedBlock>> threadBlocks = new ConcurrentDictionary<int, List<TracedBlock>>();
-
-        public TraceResult GetTraceResult()
-        {
-            throw new NotImplementedException();
-        }
-
+        private TraceResult traceResult = new TraceResult();
+        private ConcurrentDictionary<int, Stack<TracedBlock>> threadBlocks = new ConcurrentDictionary<int, Stack<TracedBlock>>();
         public void StartTrace()
         {
-            throw new NotImplementedException();
+            int threadId = System.Threading.Thread.CurrentThread.ManagedThreadId;
+            traceResult.threads.GetOrAdd(threadId, new ThreadInfo());
+            Stack<TracedBlock> threadStack = threadBlocks.GetOrAdd(threadId, new Stack<TracedBlock>());
+
+            StackTrace stackTrace = new StackTrace();
+            TracedBlock newBlock = new TracedBlock(new MethodTrace(stackTrace.GetFrame(1).GetMethod()));
+
+            UpdateParentInfo(threadStack, newBlock);
+
+            threadStack.Push(newBlock);
+
+            newBlock.watch.Restart();
+        }
+
+        private void UpdateParentInfo(Stack<TracedBlock> threadStack, TracedBlock tracedBlock)
+        {
+            if (threadStack.Count != 0)
+            {
+                MethodTrace parent = threadStack.Peek().methodTrace;
+                parent.innerCalls.Add(tracedBlock.methodTrace);
+            } else
+            {
+                int threadId = System.Threading.Thread.CurrentThread.ManagedThreadId;
+
+                ThreadInfo threadInfo;
+                traceResult.threads.TryGetValue(threadId, out threadInfo);
+
+                threadInfo.methods.Add(tracedBlock.methodTrace);
+
+            }
         }
 
         public void StopTrace()
         {
-            throw new NotImplementedException();
+            int threadId = System.Threading.Thread.CurrentThread.ManagedThreadId;
+            Stack<TracedBlock> threadStack;
+            threadBlocks.TryGetValue(threadId, out threadStack);
+
+            TracedBlock tracedBlock = threadStack.Pop();
+            tracedBlock.watch.Stop();
+
+            TimeSpan executionTime = tracedBlock.watch.Elapsed;
+            tracedBlock.methodTrace.executionTime = executionTime;
+
+            ThreadInfo threadInfo;
+            traceResult.threads.TryGetValue(threadId, out threadInfo);
+
+            threadInfo.executionTime.Add(executionTime);
         }
+
+        public TraceResult GetTraceResult()
+        {
+            return traceResult;
+        }
+
     }
 }
