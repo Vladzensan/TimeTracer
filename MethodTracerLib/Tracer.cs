b7f4af7f8bc00a3ae08ba266142df263fdd676ce
Vladzensan <vladzensan@gmail.com>
2020-09-19 19:20:16 +0300

    Initial commit

---
+++ MethodTracerLib/Tracer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace MethodTracerLib
+{
+    class Tracer : ITracer
+    {
+        private class TracedBlock
+        {
+
+        }
+
+        private ConcurrentDictionary<int, List<TracedBlock>> threadBlocks = new ConcurrentDictionary<int, List<TracedBlock>>();
+
+        public TraceResult GetTraceResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartTrace()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopTrace()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
